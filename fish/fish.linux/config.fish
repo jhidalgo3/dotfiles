0af978c0ab8577dd2c3d4976284af118588fbd0e
Jose Maria Hidalgo Garcia <jhidalgo3@gmail.com>
2016-02-14 11:34:03 +0100

    fish configuration

---
+++ fish/fish.linux/config.fish
@@ -0,0 +1,51 @@
+# Sample config
+
+set -g fish_color_user magenta
+set -g fish_color_host yellow
+set -g fish_prompt_git_status_git_dir '⚒ '  
+set -g fish_prompt_git_remote_space ' '
+
+. $HOME/.config/fish/informative_git_prompt.fish
+
+function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+
+  # User
+  set_color $fish_color_user
+  echo -n (whoami)
+  set_color normal
+
+  echo -n '@'
+
+  # Host
+  set_color $fish_color_host
+  echo -n (hostname -s)
+  set_color normal
+
+  echo -n ':'
+
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__informative_git_prompt)
+
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
+
+  echo -n '$ '
+  
+  set_color $fish_color_normal
+
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+
+  # Time
+  set_color -o black
+  echo (date +%R)
+  set_color $fish_color_normal
+
+end
